20a3daf5f24b3cabb37418b86695728bc5af1915
IrinaaMi <iro4ka.m.n@gmail.com>
2023-08-06 11:55:01 +0200

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// // // // Change code below this line
+const container = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createMarkup(arr) {
+    return arr
+        .map(
+            ({ preview, original, description }) => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+            </a>
+        </li>`
+        )
+        .join("");
+}
+
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+container.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    const target = evt.target;
+
+    if (target.classList.contains("gallery__image")) {
+        const currentItem = target.closest(".gallery__item");
+        const { source } = target.dataset;
+        const item = galleryItems.find((item) => item.original === source);
+
+        const instance = basicLightbox.create(`
+                <img src="${item.original}" alt="${item.description}" width="800" height="600">
+    
+        `);
+        instance.show();
+
+        const escapeWindow = (escEvent) => {
+            if (escEvent.key === "Escape") {
+                instance.close();
+                document.removeEventListener("keydown", escapeWindow);
+            }
+        };
+        document.addEventListener("keydown", escapeWindow);
+    }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+const container = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+    return arr
+        .map(
+            ({ preview, original, description }) =>
+            `<li class="gallery__item">
+        <a class="gallery__link" href="${original}" data-lightbox="gallery">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`
+        )
+        .join("");
+}
+container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+container.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    const target = evt.target;
+    const lightbox = new SimpleLightbox(".gallery a", {
+        captions: true,
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+    if (target.classList.contains("gallery__image")) {
+        const currentItem = target.closest(".gallery__item");
+        const { source } = target.dataset;
+        const item = galleryItems.find((item) => item.original === source);
+
+        const escapeWindow = (escEvent) => {
+            if (escEvent.key === "Escape") {
+                lightbox.close();
+                document.removeEventListener("keydown", escapeWindow);
+            }
+        };
+        document.addEventListener("keydown", escapeWindow);
+    }
+});
