5f2d51ed589f18926c44e377d8a3cf060d97b51f
IrinaaMi <iro4ka.m.n@gmail.com>
2023-08-06 12:23:29 +0200

    Update 02-lightbox.js
    виправлення помилки"Завдання 2 Бібліотека SimpleLightbox бере на себе обробку кліків по зображеннях. Ви додаєте обробку кліків ще і в коді, тому при кліку по зображенні відбувається задвоєння кліку."

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -18,26 +18,9 @@ function createMarkup(arr) {
         .join("");
 }
 container.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
-container.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    const target = evt.target;
-    const lightbox = new SimpleLightbox(".gallery a", {
-        captions: true,
-        captionsData: "alt",
-        captionDelay: 250,
-    });
-
-    if (target.classList.contains("gallery__image")) {
-        const currentItem = target.closest(".gallery__item");
-        const { source } = target.dataset;
-        const item = galleryItems.find((item) => item.original === source);
-
-        const escapeWindow = (escEvent) => {
-            if (escEvent.key === "Escape") {
-                lightbox.close();
-                document.removeEventListener("keydown", escapeWindow);
-            }
-        };
-        document.addEventListener("keydown", escapeWindow);
-    }
+const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
 });
+const { source } = target.dataset;
